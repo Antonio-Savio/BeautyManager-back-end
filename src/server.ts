16de18f199f8a381d1e5126a8e4d026f11c773e3
Antonio-Savio <antonio.savio28@gmail.com>
2025-03-25 01:04:20 -0300

    build: setting up to deploy on Vercel

--- src/server.ts
+++ src/server.ts
@@ -34,4 +34,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
 })
 
-app.listen(3333, () => console.log("Servidor online na porta 3333"));
+app.listen(process.env.PORT, () => console.log(`Servidor online na porta ${process.env.PORT}`));
